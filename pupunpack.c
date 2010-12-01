4fc3dff855d99e5e368e4b609c8e0faa1c1ef499
Sven Peter <svenpeter@gmail.com>
2010-12-01 17:03:20 +0100

    pupunpack fixes

--- pupunpack.c
+++ pupunpack.c
@@ -10,54 +10,54 @@
 
 static u8 *pup = NULL;
 static u64 n_sections;
+static u64 hdr_size;
 
-static void get_fname(char *fname, u64 idx)
-{
-	memset(fname, 0, 256);
-	if (idx == 0)
-		strcpy(fname, "version.txt");
-	else
-		snprintf(fname, 255, "section-%lld", idx);
-}
+static struct id2name_tbl t_names[] = {
+	{0x100, "version.txt"},
+	{0x101, "license.txt"},
+	{0x102, "promo_flags.txt"},
+	{0x103, "update_flags.txt"},
+	{0x104, "patch_build.txt"},
+	{0x200, "ps3swu.self"},
+	{0x201, "vsh.tar"},
+	{0x202, "dots.txt"},
+	{0x203, "patch_data.pkg"},
+	{0x300, "update_files.tar"},
+	{0, NULL}
+};
 
 static void do_section(u64 i)
 {
-	u64 unk;
+	u8 *ptr;
+	u64 entry;
 	u64 offset;
 	u64 size;
-	u64 idx;
-	u8 *hash;
-	u32 j;
-	char fname[256];
-	
-	unk = be64(pup + 48 + 32*i);
-	offset = be64(pup + 56 + 32*i);
-	size = be64(pup + 64 + 32*i);
-	idx = be64(pup + 48 + 32*n_sections + 32*i);
-	hash = pup + 56 + 32*n_sections + 32*i;
+	const char *fname;
 
-	printf(" section #%lld: unk: %llx offset: %llx size: %llx\n", idx, unk, offset, size);
-	printf("              hash:");
-	for (j = 0; j < 20; j++)
-		printf("%02x", hash[j]);
-	printf("\n");
+	ptr = pup + 0x30 + 0x20 * i;
+	entry  = be64(ptr);
+	offset = be64(ptr + 0x08);
+	size   = be64(ptr + 0x10);
 
-	get_fname(fname, idx);
+	fname = id2name(entry, t_names, NULL);
+	if (fname == NULL)
+		fail("unknown entry id: %08x_%08x", (u32)(entry >> 32), (u32)entry);
+
+	printf("unpacking %s (%08x_%08x bytes)...\n", fname, (u32)(size >> 32), (u32)size);
 	memcpy_to_file(fname, pup + offset, size);
 }
 
 static void do_pup(void)
 {
-	u64 hdr_size;
 	u64 data_size;
 	u64 i;
 	
-	n_sections = be64(pup + 24);
-	hdr_size = be64(pup + 32);
-	data_size = be64(pup + 40);
+	n_sections = be64(pup + 0x18);
+	hdr_size   = be64(pup + 0x20);
+	data_size  = be64(pup + 0x28);
 
 	printf("sections: %lld\n", n_sections);
-	printf("hdr size: %lld\n", hdr_size);
+	printf("hdr size: %llx\n", hdr_size);
 	printf("data size: %llx\n", data_size);
 
 	for (i = 0; i < n_sections; i++)
@@ -69,7 +69,7 @@ int main(int argc, char *argv[])
 	(void)argc;
 
 	if (argc < 3)
-		return 0;
+		fail("usage: pupunpack filename.pup directory");
 
 	pup = mmap_file(argv[1]);
 
